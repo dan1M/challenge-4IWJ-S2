3cbf7971a286c23e9a99c76df924999a86ce2e23
Bass913 <bastiendikiadi@outlook.fr>
2023-10-06 22:34:59 +0200

    request updated to Sequelize format for auth

--- backend/api/routes/auth.js
+++ backend/api/routes/auth.js
@@ -1,35 +1,31 @@
-const express = require("express");
-const { body } = require("express-validator/check");
+const express = require('express');
+const { body } = require('express-validator/check');
 
-const User = require("../models/user");
-const authController = require("../controllers/auth");
+const User = require('../models/user');
+const authController = require('../controllers/auth');
 
 const router = express.Router();
 
 router.put(
-  "/signup",
+  '/signup',
   [
-    body("email")
+    body('email')
       .isEmail()
-      .withMessage("Please enter a valid email.")
-      .custom((value, { req }) => {
-        return User.findOne({ email: value }).then((userDoc) => {
-          if (userDoc) {
-            return Promise.reject("E-Mail address already exists!");
-          }
-        });
+      .withMessage('Please enter a valid email.')
+      .custom(async value => {
+        const existingUser = await User.findOne({ where: { email: value } });
+        if (existingUser) {
+          throw new Error('E-mail already in use');
+        }
       })
-      .normalizeEmail(),    
-    body("password").trim().isLength({ min: 5 }),
-    body("firstname").trim().not().isEmpty(),
-    body("lastname").trim().not().isEmpty(),
-
+      .normalizeEmail(),
+    body('password').trim().isLength({ min: 5 }),
+    body('firstname').trim().not().isEmpty(),
+    body('lastname').trim().not().isEmpty(),
   ],
-  authController.signup
+  authController.signup,
 );
 
 router.post('/login', authController.login);
 
-
 module.exports = router;
-
