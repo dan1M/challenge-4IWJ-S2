74c8b43995e542e2121e9a41694e86ec592d91f0
dan1M <maneadaniel555@gmail.com>
2023-10-09 11:39:31 +0200

    fix try error auth

--- backend/api/controllers/auth.js
+++ backend/api/controllers/auth.js
@@ -1,31 +1,32 @@
-const { validationResult } = require("express-validator/check");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const { validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const User = require("../models/user");
+const User = require('../models/user');
 
 exports.signup = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error("Validation failed.");
-    error.statusCode = 422;
-    throw error;
-  }
-  const email = req.body.email;
-  const firstname = req.body.firstname;
-  const lastname = req.body.lastname;
-  const password = req.body.password;
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed.');
+      error.data = errors.array();
+      error.statusCode = 422;
+      throw error;
+    }
+    const email = req.body.email;
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const password = req.body.password;
     const hashedPw = await bcrypt.hash(password, 12);
 
     const user = await User.create({
       email: email,
       password: hashedPw,
       firstname: firstname,
-      lastname: lastname
+      lastname: lastname,
     });
-    
-    res.status(201).json({ message: "User created!", userId: user.id });
+
+    res.status(201).json({ message: 'User created!', userId: user.id });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -34,7 +35,6 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-
 exports.login = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -42,24 +42,24 @@ exports.login = async (req, res, next) => {
   try {
     const user = await User.findOne({ where: { email: email } });
     if (!user) {
-      const error = new Error("A user with this email could not be found.");
+      const error = new Error('A user with this email could not be found.');
       error.statusCode = 401;
       throw error;
     }
     loadedUser = user;
     const isEqual = await bcrypt.compare(password, user.password);
     if (!isEqual) {
-      const error = new Error("Wrong password!");
+      const error = new Error('Wrong password!');
       error.statusCode = 401;
       throw error;
     }
     const token = jwt.sign(
       {
         email: loadedUser.email,
-        userId: loadedUser.id.toString()
+        userId: loadedUser.id.toString(),
       },
-      "somesupersecretsecret",
-      { expiresIn: "1h" }
+      'somesupersecretsecret',
+      { expiresIn: '1h' },
     );
     res.status(200).json({ token: token, userId: loadedUser.id.toString() });
   } catch (err) {
@@ -69,4 +69,3 @@ exports.login = async (req, res, next) => {
     next(err);
   }
 };
-
--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const scheme = require("./util/scheme");
-const authRoutes = require("./routes/auth");
+const scheme = require('./util/scheme');
+const authRoutes = require('./routes/auth');
 
 const port = 3000;
 
@@ -15,22 +15,20 @@ app.listen(port, () => {
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE',
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-}); 
+});
 
-app.use("/auth", authRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   const status = error.statusCode || 500;
   const message = error.message;
   const data = error.data;
-  res.status(status).json({ message: message, data: data });
+  res.status(status).json({ message: message, invalid_data: data });
 });
-
-app.use(bodyParser.urlencoded({ extended: false }));
