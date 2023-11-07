45242c46c92b1d8647a19c314a0e28229a164598
dan1M <maneadaniel555@gmail.com>
2023-11-07 17:38:15 +0100

    added support to shadcn components

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,9 @@
-<script setup></script>
+<script setup lang="ts">
+import { Button } from './components/ui/button';
+</script>
 
 <template>
+  <Button>dqsdsqd</Button>
   <router-view></router-view>
 </template>
 
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
@@ -6,7 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
