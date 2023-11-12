442b1878377d191ea43088ba96396b2618d25218
Bass913 <bastiendikiadi@outlook.fr>
2023-11-12 18:42:56 +0100

    model products fixed conflict

--- backend/api/models/sql/product.js
+++ backend/api/models/sql/product.js
@@ -22,16 +22,17 @@ const Product = sequelize.define(
     category_id: {
       type: DataTypes.UUID,
       allowNull: false,
-    price: {
-      type: DataTypes.DOUBLE,
-    },
-    imageUrl: {
-      type: DataTypes.TEXT,
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 0,
+      price: {
+        type: DataTypes.DOUBLE,
+      },
+      imageUrl: {
+        type: DataTypes.TEXT,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
     },
   },
   {
